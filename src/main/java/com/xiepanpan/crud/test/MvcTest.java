a96eb9bf41f851dbbc0d79449037df45b1fe63c2
xiepanpan <34202282+xiepanpan@users.noreply.github.com>
2018-10-16 21:14:24 +0800

    测试类

---
+++ src/main/java/com/xiepanpan/crud/test/MvcTest.java
@@ -0,0 +1,22 @@
+package com.xiepanpan.crud.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:applicationContext.xml","file:src/main/webapp/WEB-INF/dispatcherServlet-servlet.xml"})
+public class MvcTest {
+
+    //传入springmvc的ioc
+    @Autowired
+    WebApplicationContext context;
+    //虚拟mvc请求 获取到处理结果
+    MockMvc mockMvc;
+}
+
